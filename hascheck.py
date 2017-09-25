cd44f1bfb1209a6b0024c88caf9f694258e618e5
josduj <josduj@users.noreply.github.com>
2017-09-25 10:18:53 +0200

    refactoring

--- hascheck.py
+++ hascheck.py
@@ -10,11 +10,51 @@ def get_popup_content(items):
 		return "Might be a word!"
 	elif "!IME!" in items:
 		return "Might be a name!"
+	elif "!TUĐICA!" in items:
+		return "Might be foreign word!"
 	else:
 		html = "<b>Suggestions: </b>"
 		for i in items:
 			html += "<li><a href='{0}'>{0}</a></li>".format(i)
 		return html
+
+def get_flags(highlight, underline):
+	if highlight == "underline":
+		clean = sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE
+		if underline == "solid":
+			return clean | sublime.DRAW_SOLID_UNDERLINE
+		elif underline == "stippled":
+			return clean | sublime.DRAW_STIPPLED_UNDERLINE
+		else:
+			return clean | sublime.DRAW_SQUIGGLY_UNDERLINE
+	elif highlight == "outline":
+		return sublime.DRAW_NO_FILL
+	else:
+		return 0
+
+def replace_text(view, region, text):
+	args = {
+		"begin"	: region.a,
+		"end"	: region.b,
+		"text"	: text
+	}
+	view.run_command("hascheck_replace_text", args)
+	view.hide_popup()
+
+def show_suggestions_menu(view, region):
+	def popup():
+		view.show_popup_menu(suggestions[text], lambda x:
+			x == -1 or replace_text(view, region, suggestions[text][x]))
+	text = view.substr(region)
+	view.show_at_center(region)
+	sublime.Selection.clear(view)
+	sublime.Selection.add(view, region)
+	sublime.set_timeout_async(popup, 200)
+
+def remove_region(view, region):
+	regions = view.get_regions("hascheck_errors")
+	regions.remove(region)
+	view.add_regions("hascheck_errors", regions, SCOPE, ICON, FLAGS)
 
 class HascheckListener(sublime_plugin.ViewEventListener):
 	def on_hover(self, point, hover_zone):
@@ -22,38 +62,22 @@ class HascheckListener(sublime_plugin.ViewEventListener):
 			selection = self.view.word(point)
 			text = self.view.substr(selection)
 			if text in suggestions.keys():
-				def select(text):
-					args = {
-						"begin"	: selection.a,
-						"end"	: selection.b,
-						"text"	: text
-					}
-					self.view.run_command("hascheck_replace_text", args)
-					self.view.hide_popup()
-					
 				self.view.show_popup(
 					get_popup_content(suggestions[text]),
 					sublime.HIDE_ON_MOUSE_MOVE_AWAY,
 					point,
-					on_navigate = select
+					on_navigate = lambda x: replace_text(self.view, selection, x)
 				)
 
 	def on_modified_async(self):
 		regions = self.view.get_regions("hascheck_errors")
 		for r in regions:
 			if self.view.substr(r) not in suggestions.keys():
-				self.view.run_command("hascheck_remove_region", {"begin": r.a, "end": r.b})
+				remove_region(self.view, r)
 
 class HascheckReplaceText(sublime_plugin.TextCommand):
 	def run(self, edit, begin, end, text):
 		self.view.replace(edit, sublime.Region(begin, end), text)
-		return
-
-class HascheckRemoveRegion(sublime_plugin.TextCommand):
-	def run(self, edit, begin, end):
-		regions = self.view.get_regions("hascheck_errors")
-		regions.remove(sublime.Region(begin, end))
-		self.view.add_regions("hascheck_errors", regions, SCOPE, ICON, FLAGS)
 		return
 
 class HascheckNextErrorCommand(sublime_plugin.TextCommand):
@@ -65,9 +89,7 @@ class HascheckNextErrorCommand(sublime_plugin.TextCommand):
 		regions = self.view.get_regions("hascheck_errors")
 		for reg in regions:
 			if self.view.sel()[0].end() < reg.a:
-				sublime.Selection.clear(self.view)
-				sublime.Selection.add(self.view, reg)
-				self.view.show_at_center(reg)
+				show_suggestions_menu(self.view, reg)
 				return
 
 class HascheckPrevErrorCommand(sublime_plugin.TextCommand):
@@ -79,9 +101,7 @@ class HascheckPrevErrorCommand(sublime_plugin.TextCommand):
 		regions = self.view.get_regions("hascheck_errors")
 		for reg in reversed(regions):
 			if self.view.sel()[0].end() > reg.b:
-				sublime.Selection.clear(self.view)
-				sublime.Selection.add(self.view, reg)
-				self.view.show_at_center(reg)
+				show_suggestions_menu(self.view, reg)
 				return
 
 class HascheckCommand(sublime_plugin.TextCommand):
@@ -110,20 +130,6 @@ class HascheckCommand(sublime_plugin.TextCommand):
 			return
 
 		sublime.set_timeout_async(fetch_async, 0)
-
-def get_flags(highlight, underline):
-	if highlight == "underline":
-		clean = sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE
-		if underline == "solid":
-			return clean | sublime.DRAW_SOLID_UNDERLINE
-		elif underline == "stippled":
-			return clean | sublime.DRAW_STIPPLED_UNDERLINE
-		else:
-			return clean | sublime.DRAW_SQUIGGLY_UNDERLINE
-	elif highlight == "outline":
-		return sublime.DRAW_NO_FILL
-	else:
-		return 0
 
 def plugin_loaded():
 	global FLAGS, ICON, SCOPE
